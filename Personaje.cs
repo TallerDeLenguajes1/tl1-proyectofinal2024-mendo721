253a87ca9f9923f946dcce4cb99e453945007f9b
Rodrigo <rodrigomendoza721@gmail.com>
2024-07-26 13:15:24 -0300

    correccion de personajes

--- Personaje.cs
+++ Personaje.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
+
 namespace Personajes
 {
     public class Personaje
@@ -19,20 +21,20 @@ namespace Personajes
         public int Armadura { get; set; }
         public int Salud { get; set; } = 100;
 
+        public Personaje(string tipo, string nombre, string apodo, DateTime fechaDeNacimiento, int velocidad, int destreza, int fuerza, int nivel, int armadura)
+        {
+            Tipo = tipo;
+            Nombre = nombre;
+            Apodo = apodo;
+            FechaDeNacimiento = fechaDeNacimiento;
+            Velocidad = velocidad;
+            Destreza = destreza;
+            Fuerza = fuerza;
+            Nivel = nivel;
+            Armadura = armadura;
+        }
     }
-    
-    public Personaje(string tipo, string nombre, string apodo, DateTime fechaDeNacimiento, int velocidad, int destreza, int fuerza, int nivel, int armadura)
-    {
-        Tipo = tipo;
-        Nombre = nombre;
-        Apodo = apodo;
-        FechaDeNacimiento = fechaDeNacimiento;
-        Velocidad = velocidad;
-        Destreza = destreza;
-        Fuerza = fuerza;
-        Nivel = nivel;
-        Armadura = armadura;
-    }
+
     public class Jugador : Personaje
     {
         public Jugador(string tipo, string nombre, string apodo, DateTime fechaDeNacimiento, int velocidad, int destreza, int fuerza, int nivel, int armadura)
@@ -48,38 +50,49 @@ namespace Personajes
         {
         }
     }
-    //defino una clase para poder crear una lista con los tipos, nombres y apodos vinculados
-    public class EnemigosPredefinidos{
+
+    // Clase para definir enemigos predefinidos
+    public class EnemigoPredefinido
+    {
         public string Tipo { get; set; }
         public string Nombre { get; set; }
         public string Apodo { get; set; }
-        
+
+        public EnemigoPredefinido(string tipo, string nombre, string apodo)
+        {
+            Tipo = tipo;
+            Nombre = nombre;
+            Apodo = apodo;
+        }
     }
 
-    public class FabricaDePersonajes{
-        private static Ramdom randon = new Ramdom();
-        private static List<Personaje> personajesPredefinidos = new List<Personaje>{
+    public class FabricaDePersonajes
+    {
+        private static Random random = new Random();
+        private static List<Personaje> personajesPredefinidos = new List<Personaje>
+        {
             new Jugador("Guerrero", "Arthur", "El Bravo", new DateTime(2000, 1, 1), 7, 4, 9, 5, 8),
             new Jugador("Mago", "Merlin", "El Astuto", new DateTime(1950, 5, 10), 5, 5, 3, 7, 6),
             new Jugador("Arquero", "Legolas", "El Preciso", new DateTime(1985, 11, 20), 9, 3, 7, 4, 7)
         };
 
-        public static list<Personaje> ObtenerPersonajes(){
+        public static List<Personaje> ObtenerPersonajes()
+        {
             return personajesPredefinidos;
         }
-        
-        //lista de enemigos con sus tipos, nombres y apodos vinculados
-        private static List<EnemigosPredefinidos> enemigosPredefinidos = new List<EnemigosPredefinidos>{
-            new EnemigosPredefinidos("Granader", "Ramon", "El malo"),
-            new EnemigosPredefinidos("Artillero", "Julio", "El rudo"),
-            new EnemigosPredefinidos("Comandante", "Carlos", "El debil"),
+
+        private static List<EnemigoPredefinido> enemigosPredefinidos = new List<EnemigoPredefinido>
+        {
+            new EnemigoPredefinido("Granadero", "Ramon", "El Malo"),
+            new EnemigoPredefinido("Artillero", "Julio", "El Rudo"),
+            new EnemigoPredefinido("Comandante", "Carlos", "El Debil")
         };
 
         //Se utiliza para no repetir los nombres de los personajes HashSet se utiliza para que no se repita elementos en la lista
         private static HashSet<string> nombresUtilizados = new HashSet<string>();
 
-        public static Enemigo CrearEnemigoAleatorio(){
-            //Utilizo la funcion para seleccionar enemigos predefinidos
+        public static Enemigo CrearEnemigoAleatorio()
+        {
             EnemigoPredefinido enemigoPredefinido = SeleccionarEnemigoPredefinido();
 
             // Le adjudico atributos aleatorios a los enemigos
@@ -97,7 +110,9 @@ namespace Personajes
 
             return enemigo;
         }
-        private static EnemigoPredefinido SeleccionarEnemigoPredefinido(){
+
+        private static EnemigoPredefinido SeleccionarEnemigoPredefinido()
+        {
             List<EnemigoPredefinido> enemigosDisponibles = enemigosPredefinidos.FindAll(enemigo => !nombresUtilizados.Contains(enemigo.Nombre));
             if (enemigosDisponibles.Count == 0)
             {
---
+++ PersonajesJson.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using Personajes;
+
+namespace PersonajesJson
+{
+    public class PersonajesJson
+    {
+        //Metodo para guardar los personajes en formato Json
+        public static void GuardarPersonajes(List<Personaje> personajes, string NombreArchivo)
+        {
+            //agrego la opcion para generar un formato Json mas legible
+            var OpcionesJson = new JsonSerializerOptions {WriteIndented = true};
+            //serializo y guardo con las configuraciones del formato opcionesJson
+            string Json =  JsonSerializer.Serialize(personajes, OpcionesJson);
+            //Se guarda en el archivo el Json
+            File.WriteAllText(NombreArchivo, Json);
+        }
+
+        //Metodo para leer los personajes en Json en el archivo
+        public static List<Personaje> LeerPersonajes(string NombreArchivo)
+        {
+            if (!File.Exists(NombreArchivo))
+            {
+                Console.WriteLine("\nEl archivo no existe");
+                return new List<Personaje>();
+            }
+            string Json = File.ReadAllText(NombreArchivo);
+            return JsonSerializer.Deserialize<List<Personaje>>(Json);
+        }
+
+        public static bool Existe(string NombreArchivo)
+        {
+            if (File.Exists(NombreArchivo))
+            {
+                string Json = File.ReadAllText(NombreArchivo);
+                return !string.IsNullOrEmpty(Json);
+            }
+            return false;
+        }
+    }
+}
