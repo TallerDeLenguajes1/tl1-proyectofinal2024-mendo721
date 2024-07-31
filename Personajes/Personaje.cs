7ec1ea23812bbd3e5ded2d65e16b41d8461c4953
Rodrigo <rodrigomendoza721@gmail.com>
2024-07-31 00:28:17 -0300

    arreglo de edad

--- Personajes/Personaje.cs
+++ Personajes/Personaje.cs
@@ -21,12 +21,13 @@ namespace Personajes
         public int Armadura { get; set; }
         public int Salud { get; set; } = 100;
 
-        public Personaje(string tipo, string nombre, string apodo, DateTime fechaDeNacimiento, int velocidad, int destreza, int fuerza, int nivel, int armadura)
+        public Personaje(string tipo, string nombre, string apodo, DateTime fechaDeNacimiento, int edad, int velocidad, int destreza, int fuerza, int nivel, int armadura)
         {
             Tipo = tipo;
             Nombre = nombre;
             Apodo = apodo;
             FechaDeNacimiento = fechaDeNacimiento;
+            Edad = edad;
             Velocidad = velocidad;
             Destreza = destreza;
             Fuerza = fuerza;
@@ -37,16 +38,16 @@ namespace Personajes
 
     public class Jugador : Personaje
     {
-        public Jugador(string tipo, string nombre, string apodo, DateTime fechaDeNacimiento, int velocidad, int destreza, int fuerza, int nivel, int armadura)
-            : base(tipo, nombre, apodo, fechaDeNacimiento, velocidad, destreza, fuerza, nivel, armadura)
+        public Jugador(string tipo, string nombre, string apodo, DateTime fechaDeNacimiento, int edad, int velocidad, int destreza, int fuerza, int nivel, int armadura)
+            : base(tipo, nombre, apodo, fechaDeNacimiento, edad, velocidad, destreza, fuerza, nivel, armadura)
         {
         }
     }
 
     public class Enemigo : Personaje
     {
-        public Enemigo(string tipo, string nombre, string apodo, DateTime fechaDeNacimiento, int velocidad, int destreza, int fuerza, int nivel, int armadura)
-            : base(tipo, nombre, apodo, fechaDeNacimiento, velocidad, destreza, fuerza, nivel, armadura)
+        public Enemigo(string tipo, string nombre, string apodo, DateTime fechaDeNacimiento, int edad, int velocidad, int destreza, int fuerza, int nivel, int armadura)
+            : base(tipo, nombre, apodo, fechaDeNacimiento, edad, velocidad, destreza, fuerza, nivel, armadura)
         {
         }
     }
@@ -71,9 +72,9 @@ namespace Personajes
         private static Random random = new Random();
         private static List<Personaje> personajesPredefinidos = new List<Personaje>
         {
-            new Jugador("Guerrero", "Arthur", "El Bravo", new DateTime(2000, 1, 1), 7, 4, 9, 5, 8),
-            new Jugador("Mago", "Merlin", "El Astuto", new DateTime(1950, 5, 10), 5, 5, 3, 7, 6),
-            new Jugador("Arquero", "Legolas", "El Preciso", new DateTime(1985, 11, 20), 9, 3, 7, 4, 7)
+            new Jugador("Guerrero", "Arthur", "El Bravo", new DateTime(2000, 1, 1), 24, 7, 4, 9, 5, 8),
+            new Jugador("Mago", "Merlin", "El Astuto", new DateTime(1950, 5, 10), 74, 5, 5, 3, 7, 6),
+            new Jugador("Arquero", "Legolas", "El Preciso", new DateTime(1985, 11, 20), 38, 9, 3, 7, 4, 7)
         };
 
         public static List<Personaje> ObtenerPersonajes()
@@ -85,7 +86,16 @@ namespace Personajes
         {
             new EnemigoPredefinido("Granadero", "Ramon", "El Malo"),
             new EnemigoPredefinido("Artillero", "Julio", "El Rudo"),
-            new EnemigoPredefinido("Comandante", "Carlos", "El Debil")
+            new EnemigoPredefinido("Comandante", "Carlos", "El Debil"),
+            new EnemigoPredefinido("Razo", "Miguel", "El Debil"),
+            new EnemigoPredefinido("Razo", "David", "El Debil"),
+            new EnemigoPredefinido("Razo", "Angel", "El Debil"),
+            new EnemigoPredefinido("Razo", "Ramiro", "El Debil"),
+            new EnemigoPredefinido("Razo", "Tobias", "El Debil"),
+            new EnemigoPredefinido("Razo", "Antonio", "El Debil"),
+            new EnemigoPredefinido("Razo", "Michael", "El Debil"),
+            new EnemigoPredefinido("Razo", "arito", "El Debil"),
+            new EnemigoPredefinido("Razo", "Lauty", "El Debil"),
         };
 
         //Se utiliza para no repetir los nombres de los personajes HashSet se utiliza para que no se repita elementos en la lista
@@ -102,8 +112,9 @@ namespace Personajes
             int fuerza = random.Next(1, 11);
             int nivel = random.Next(1, 11);
             int armadura = random.Next(1, 11);
+            int edad = CalcularEdad(fechaDeNacimiento);
 
-            Enemigo enemigo = new Enemigo(enemigoPredefinido.Tipo, enemigoPredefinido.Nombre, enemigoPredefinido.Apodo, fechaDeNacimiento, velocidad, destreza, fuerza, nivel, armadura);
+            Enemigo enemigo = new Enemigo(enemigoPredefinido.Tipo, enemigoPredefinido.Nombre, enemigoPredefinido.Apodo, fechaDeNacimiento, edad, velocidad, destreza, fuerza, nivel, armadura);
 
             // Registrar el nombre como utilizado para que no se repita
             nombresUtilizados.Add(enemigoPredefinido.Nombre);
@@ -127,6 +138,14 @@ namespace Personajes
             int month = random.Next(1, 13);
             int day = random.Next(1, DateTime.DaysInMonth(year, month) + 1);
             return new DateTime(year, month, day);
+        }
+
+        private static int CalcularEdad(DateTime fechaDeNacimiento)
+        {
+            var hoy = DateTime.Today;
+            int edad = hoy.Year - fechaDeNacimiento.Year;
+            if (fechaDeNacimiento.Date > hoy.AddYears(-edad)) edad--;
+            return edad;
         }
     }
 }
--- obj/Debug/net8.0/tl1-proyectofinal2024-mendo721.AssemblyInfo.cs
+++ obj/Debug/net8.0/tl1-proyectofinal2024-mendo721.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tl1-proyectofinal2024-mendo721")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed5531caf41273b5ed315c1920b5f0589e7f8103")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc67c70e1702611f2b6aa848d250c8bf44957c59")]
 [assembly: System.Reflection.AssemblyProductAttribute("tl1-proyectofinal2024-mendo721")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tl1-proyectofinal2024-mendo721")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
